2fa357f300b5a4ad37aefae1c381676ee52d9977
victor <victor.osornio@emqu.net>
2018-04-19 21:23:29 -0500

    ya todo esta conectado

---
+++ prueba/src/test/java/TestCuentaLineas.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.assertTrue;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+public class TestCuentaLineas{
+    cuentaLineas bla = new cuentaLineas();
+    @Test
+    public void prueba1(){
+    System.out.println("Estoy en prueba uno");
+    }
+}
